031904422b9fa3df72fc852b110f7a31268e9d26
Xziknight <dorian.dubois.dd@gmail.com>
2021-03-19 11:12:59 +0100

    deuxiéme exercice terminée

--- exo 2.php
+++ exo 2.php
@@ -1,21 +1,10 @@
 <?php
-
-$z = 0;
-
-while ($z <= 10000) {
-    if($z % 2 === 0){
-    $z++;
-}  
-    else
-{
-   echo $z;
-   echo '<br />';
-    $z++; 
+$i=1;
+for($i; $i <= 100;$i++){
+    echo 'je dois mon cours par coeur <br />';
+    
+    
 }
-    
-} 
-    
-
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
