d96ae6604bd1493a1a819f747b81979ed0e32497
Xziknight <dorian.dubois.dd@gmail.com>
2021-03-19 10:45:04 +0100

    création du depot et du permièr exercice

---
+++ exo 1.php
@@ -0,0 +1,17 @@
+<?php
+
+$i = 0;
+
+while (i <= 10000) {
+    
+        
+    
+} 
+    
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
