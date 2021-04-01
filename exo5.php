d583a8e03a8ae6cdeafda53ba83ec42dffe056ed
theknight <theknight@LAPTOP-1FHFMFPM>
2021-04-01 22:43:15 +0200

    fin de la table de 13

--- exo5.php
+++ exo5.php
@@ -1,13 +1,23 @@
 <?php
-for ($i = 1; $i <= 13; $i++)
-{
-    echo $i,' ';
-    for ($j = 1; $j <= 13; $j++)
+
+for ($z = 1; $z <= 13; $z++) {
+    if ($z === 1) {
+        echo "\t",$z, ' ';
+    }
+    else
     {
-        echo$i*$j, ' ';
+        echo $z, ' ';
+    }
+}
+echo '<br>';
+for ($i = 1; $i <= 13; $i++) {
+    echo $i, ' ';
+    for ($j = 1; $j <= 13; $j++) {
+        echo$i * $j, ' ';
     }
     echo '<br>';
 }
+
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
---
+++ exo5V2.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link href="exo5V2.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <table>
+            <tr>
+                
+                    <?php
+                    for ($z = 0; $z <= 13; $z++) {
+                        ?>
+                <td>
+<?php
+
+                        if ($z == 0) {
+                            echo ' ';
+                        } else {
+                            echo $z;
+                        }
+                    }
+                    ?>
+                </td>
+            
+            </tr>
+            <tr>
+                <?php
+                for ($i = 0; $i <= 13; $i++) {
+                   
+                        
+               
+                   ?>
+            <td>
+                        <?php
+                        if($i == 0)
+                        {
+                            for ($j = 1; $j <= 13; $j++) {
+
+                            echo$i+1 * $j, '<br>';
+                        }
+                        }
+
+                        else
+                        {
+                        
+                        for ($j = 1; $j <= 13; $j++) {
+
+                            echo$i * $j, '<br>';
+                        }
+                }
+                }
+                    ?>
+                </td>
+            </tr>
+
+
+        </table>
+    </body>
+</html>
