bb8dfb7f773acee9df0a15d291dc553e6436634e
theknight <theknight@LAPTOP-1FHFMFPM>
2021-04-01 15:30:32 +0200

    un bon debut pour la table de 13

---
+++ exo5.php
@@ -0,0 +1,16 @@
+<?php
+for ($i = 1; $i <= 13; $i++)
+{
+    echo $i,' ';
+    for ($j = 1; $j <= 13; $j++)
+    {
+        echo$i*$j, ' ';
+    }
+    echo '<br>';
+}
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
